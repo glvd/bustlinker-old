9a8dc2358717b581a43a110a5823781c68dbaeb9
godcong <jumbycc@163.com>
2020-08-26 18:25:57 +0800

    add link

--- link/pinning.go
+++ link/pinning.go
@@ -1,18 +1,25 @@
 package link
 
 import (
+	"context"
 	"github.com/glvd/bustlinker/core"
+	"github.com/glvd/bustlinker/core/coreapi"
+	"go.uber.org/atomic"
 	"sync"
 )
 
 type Pinning interface {
 	Get() []string
 	Clear()
-	Add(pin string)
+	AddSync(pin string)
 	Set(pins []string)
 }
 
 type pinning struct {
+	ctx      context.Context
+	cancel   context.CancelFunc
+	running  *atomic.Bool
+	syncing  *sync.Pool
 	node     *core.IpfsNode
 	pins     map[string]bool
 	pinsLock *sync.RWMutex
@@ -37,6 +44,10 @@ func (p *pinning) Clear() {
 	p.pinsLock.Unlock()
 }
 
+func (p *pinning) AddSync(pin string) {
+	p.syncing.Put(pin)
+}
+
 func (p *pinning) Add(pin string) {
 	p.pinsLock.Lock()
 	p.pins[pin] = true
@@ -53,10 +64,35 @@ func (p *pinning) Set(pins []string) {
 	p.pinsLock.Unlock()
 }
 
+func (p *pinning) Pause() {
+	p.running.Store(false)
+}
+
+func (p *pinning) Resume() {
+	if p.running.CAS(false, true) {
+		go p.run()
+	}
+}
+
+func (p *pinning) run() {
+	p.ctx, p.cancel = context.WithCancel(context.TODO())
+	api, err := coreapi.NewCoreAPI(p.node)
+	if err != nil {
+		log.Error("run pinning failed:", err)
+		return
+	}
+	for p.running.Load() {
+		api.Pin()
+	}
+}
+
 func newPinning(node *core.IpfsNode) Pinning {
-	return &pinning{
+	p := &pinning{
+		running:  atomic.NewBool(true),
 		node:     node,
 		pins:     make(map[string]bool),
 		pinsLock: &sync.RWMutex{},
 	}
+	go p.run()
+	return p
 }
